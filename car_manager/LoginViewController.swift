6ee54f5b17dfe83a9722b7885cddc0dc4d9c8763
muziruo <1149354821@qq.com>
2017-12-08 21:03:19 +0800

    Test update

---
+++ car_manager/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  car_manager
+//
+//  Created by 李祎喆 on 2017/12/8.
+//  Copyright © 2017年 李祎喆. All rights reserved.
+//  登录页面
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var UserPassword: UITextField!
+    @IBOutlet weak var UserName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
