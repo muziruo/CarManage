a9d2faadb5aef7705a1abd2e4d9af627e539117a
muziruo <1149354821@qq.com>
2017-12-11 20:08:47 +0800

    update

---
+++ car_manager/SearchModule/SearchMenuViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SearchMenuViewController.swift
+//  car_manager
+//
+//  Created by 李祎喆 on 2017/12/11.
+//  Copyright © 2017年 李祎喆. All rights reserved.
+//
+
+import UIKit
+
+class SearchMenuViewController: UIViewController ,UITableViewDelegate ,UITableViewDataSource{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
