5b5c2d4dee0bc8c82907188b1f51119a943be841
muziruo <1149354821@qq.com>
2017-12-15 08:16:21 +0800

    update

--- car_manager/SearchModule/SearchMenuViewController.swift
+++ car_manager/SearchModule/SearchMenuViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class SearchMenuViewController: UIViewController ,UITableViewDelegate ,UITableViewDataSource{
 
+    @IBOutlet weak var SearchTableView: UITableView!
+    
     let functionmenu:[String] = ["车辆信息查询","人员信息查询"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        SearchTableView.tableFooterView = UIView(frame: .zero)
         // Do any additional setup after loading the view.
     }
 
@@ -28,13 +31,24 @@ class SearchMenuViewController: UIViewController ,UITableViewDelegate ,UITableVi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return functionmenu.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "menucell", for: indexPath) as! MenuTableViewCell
         cell.textLabel?.text = functionmenu[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            performSegue(withIdentifier: "GoToPersonSearch", sender: nil)
+        case 1:
+            performSegue(withIdentifier: "GoToCarManage", sender: nil)
+        default:
+            print("进入车辆管理系统")
+        }
     }
     /*
     // MARK: - Navigation
