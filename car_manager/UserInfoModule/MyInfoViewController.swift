36d7b06034a25d8c17b2793f7b723230afd63a25
muziruo <1149354821@qq.com>
2017-12-27 11:04:36 +0800

    update

--- car_manager/UserInfoModule/MyInfoViewController.swift
+++ car_manager/UserInfoModule/MyInfoViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class MyInfoViewController: UIViewController {
+class MyInfoViewController: UIViewController ,UITableViewDataSource ,UITableViewDelegate{
 
+    @IBOutlet weak var UserImage: UIImageView!
+    @IBOutlet weak var UserName: UILabel!
+    @IBOutlet weak var UserNum: UILabel!
+    @IBOutlet weak var UserPost: UILabel!
+    @IBOutlet weak var LogoutButton: UIButton!
+    @IBOutlet weak var FunctionTable: UITableView!
+    
+    let functionlist = [["开发者网站"],["使用反馈"],["电话本"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        LogoutButton.layer.borderColor = UIColor.red.cgColor
+        LogoutButton.layer.borderWidth = 1.5
+        
+        FunctionTable.tableFooterView = UIView(frame: .zero)
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +35,25 @@ class MyInfoViewController: UIViewController {
     }
     
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FunctionCell", for: indexPath)
+        cell.textLabel?.font = UIFont(name: "Avenir-Light", size: 15)
+        cell.textLabel?.text = functionlist[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
     /*
     // MARK: - Navigation
 
