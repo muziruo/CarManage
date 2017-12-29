4ee4ea21103665412167663be38a50319daf8c27
luoqin <2568228269@qq.com>
2017-12-30 00:39:01 +0800

    right

--- car_manager/ManageModule/AddRecordingController.swift
+++ car_manager/ManageModule/AddRecordingController.swift
@@ -1,37 +1,27 @@
 //
-//  ManageTableViewController.swift
+//  AddRecordingController.swift
 //  car_manager
 //
-//  Created by 沐阳 on 2017/12/15.
+//  Created by 沐阳 on 2017/12/29.
 //  Copyright © 2017年 李祎喆. All rights reserved.
 //
 
 import UIKit
 
-class ManageTableViewController: UITableViewController {
+class AddRecordingController: UITableViewController {
     
-    var functions = [["添加教职工","添加车辆","添加用户","添加单位","添加通行证","添加黑名单"],["删除教职工","删除用户","移除黑名单"]]
-    let Kinds = ["staff","car","user","post","passcard","blacklist"]
-    let delete = ["deleteStaff","deleteUser","deleteBlackList"]
-    
+    var currentType = "inSchool"
+    let recordingType = ["添加进校记录","添加出校记录","添加违章记录"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.tableFooterView = UIView(frame: .zero)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        tableView.tableFooterView = UIView(frame: .zero)
-//        let type = UserDefaults.standard.value(forKey: "usertype")
-//        if type == 1{
-//            let view
-//        }
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 10
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,75 +33,40 @@ class ManageTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return functions.count
+        return 1
     }
-    
-    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return functions[section].count
+        return recordingType.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ManageCell", for: indexPath) as! ManageTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "addRecording", for: indexPath)
 
         // Configure the cell...
-        cell.label?.text = functions[indexPath.section][indexPath.row]
-        cell.label.font = UIFont(name: "Avenir-Light", size: 16)
+        cell.textLabel?.text = recordingType[indexPath.row]
+        if indexPath.row != 0 {
+            cell.accessoryType = .disclosureIndicator
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 0{
-            switch indexPath.row {
-            case 0:
-                performSegue(withIdentifier: "addStaffSegue", sender: nil)
-                break
-            case 1:
-                performSegue(withIdentifier: "addUserSegue", sender: nil)
-                break
-            case 2:
-                performSegue(withIdentifier: "addUserSegue", sender: nil)
-                break
-            case 3:
-                performSegue(withIdentifier: "addUserSegue", sender: nil)
-                break
-            case 4:
-                performSegue(withIdentifier: "addUserSegue", sender: nil)
-            case 5:
-                performSegue(withIdentifier: "addUserSegue", sender: nil)
-                break
-            default:
-                break
-            }
-        }else{
-            switch indexPath.row{
-            case 0,1:
-                performSegue(withIdentifier: "deleteSegue", sender: nil)
-                break
-            case 2:
-                performSegue(withIdentifier: "deleteBlackListSegue", sender: nil)
-                break
-            default:
-                break
-            }
+        switch indexPath.row {
+        case 0:
+            //添加进校记录
+            break
+        case 1,2:
+            performSegue(withIdentifier: "recording", sender: nil)
+            break
+        default:
+            break
         }
-        tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "addUserSegue" {
-            let dest = segue.destination as! AddUserTableViewController
-            dest.currentType = Kinds[(tableView.indexPathForSelectedRow?.row)!]
-        }else if segue.identifier == "deleteSegue"{
-            let dest = segue.destination as! DeleteUserController
-            dest.currntType = delete[(tableView.indexPathForSelectedRow?.row)!]
-        }
-    }
 
-    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
